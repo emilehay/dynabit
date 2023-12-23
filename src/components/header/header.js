e1b577e0e492555536938046fa63d268aa60d676
emilehay <emilehay@gmail.com>
2023-12-23 16:22:17 +0200

    feat: initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,8 @@
+import styles from './header.module.scss'
+
+export const Header = () => {
+    return <header className={styles.header}>
+        <h1>Hi, Emile 👋</h1>
+        <h5>Good luck for the day!</h5>
+    </header>
+}
