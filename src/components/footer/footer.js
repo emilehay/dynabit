69933344407dc404b8636d7280329b8c878c4dba
emilehay <emilehay@gmail.com>
2023-12-24 10:11:59 +0200

    feat: made svg colors more dynamic, replaced plus icon with chart and fixed footer styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,7 @@ import calendarIconActive from "../../assets/icons/calendar-filled.svg"
 import homeIcon from "../../assets/icons/home.svg";
 import homeIconActive from "../../assets/icons/home-filled.svg";
 import listIcon from "../../assets/icons/list.svg";
-import plusIcon from "../../assets/icons/circle-plus.svg";
+import chartIcon from "../../assets/icons/chart.svg";
 import plusIconActive from "../../assets/icons/circle-plus-filled.svg";
 import settingsIcon from "../../assets/icons/cog.svg";
 import settingsIconActive from "../../assets/icons/cog-filled.svg";
@@ -17,8 +17,8 @@ export const Footer = () => {
     <footer className={styles.footer}>
       <NavButton active={true} icon={homeIcon} icon_active={homeIconActive} />
       <NavButton icon={listIcon} icon_active={listIcon} />
-      <NavButton icon={plusIcon} icon_active={plusIconActive} />
       <NavButton icon={calendarIcon} icon_active={calendarIconActive} />
+      <NavButton icon={chartIcon} icon_active={chartIcon} />
       <NavButton icon={settingsIcon} icon_active={settingsIconActive} />
     </footer>
   );
